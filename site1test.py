c6b1eef46f0c0b1b2a95d4d54d15753bad4d5857
nrwoodward <68925257+nrwoodward@users.noreply.github.com>
2022-02-06 11:16:50 -0500

    added test

--- site1test.py
+++ site1test.py
@@ -1,10 +1,18 @@
+import Battery_Test_Methods as BTM
+
+
 from pickle import FALSE, TRUE
 import config
 import time
 
 
 def runtest():
-    config.IDsite1 = config.IDscale
-    config.IDscale = "--"
-    config.scalevalue = "--"
-    config.site1state = TRUE
+    # config.IDsite1 = config.IDscale
+    # config.IDscale = "--"
+    # config.scalevalue = "--"
+    # config.site1state = TRUE
+    #
+    # BTM.meas_VOC()
+
+
+
