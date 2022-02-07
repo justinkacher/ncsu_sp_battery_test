8d4e08087aa6883c0c99e09e75626015902ee17d
nrwoodward <68925257+nrwoodward@users.noreply.github.com>
2022-02-07 17:54:49 -0500

    Finished method prodecure

--- site1test.py
+++ site1test.py
@@ -1,17 +1,42 @@
-#import Battery_Test_Methods as BTM
-
+import Battery_Test_Methods as BTM
 
 from pickle import FALSE, TRUE
+import pandas as pd
 import config
 import time
 
 
 def runtest():
-    #print('run test')
     config.IDsite1 = config.IDscale
     config.IDscale = "--"
     config.scalevalue = "--"
     config.site1state = TRUE
-    #
-    # BTM.meas_VOC()
 
+    batterySite = 1
+
+    battery_dict = {'Cell ID': site1state, 'Battery Test Holder': batterySite}
+
+    # turn ON test site led
+    BTM.start_test_LED(batterySite)
+
+    # get the mean Voc
+    voc = BTM.meas_VOC(batterySite)
+    battery_dict['Voc'] = voc
+
+    # get the mean DC Impedance
+    impedance = BTM.dc_Impedance(batterySite)
+    battery_dict['Impedance'] = impedance
+
+    # perform ratio capacity testing
+    # voltage is what we are interested in. Current should be constant
+    currentL, voltageL, measTimeL = BTM.ratio_Capacity_BK8502(batterySite)
+    battery_dict.update({'Capacity measurment times': measTimeL, 'Capacity voltage measurments': voltageL, 'Capacity current': currentL})
+
+    # turn OFF test side led
+    finish_test_LED(batterySite)
+
+    # turn dictionary to dataframe
+    # uses series so columns can be of differnt length
+    fileFolder = 'C:/Users/nwoodwa/Desktop/SolarPack/'
+    df_battery_dict = pd.DataFrame({key: pd.Series(value) for key, value in battery_dict.items()})
+    df_battery_dict.to_excel(fileFolder+'Test cell ' + str(site1state) + '.xlsx')
