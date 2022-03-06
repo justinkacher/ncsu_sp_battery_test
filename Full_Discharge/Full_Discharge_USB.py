6bdf6e1e4f1ba3341b45ec9b1447c2bc0d02fbe3
nrwoodward <68925257+nrwoodward@users.noreply.github.com>
2022-03-06 11:32:26 -0500

    test upload

--- Full_Discharge/Full_Discharge_USB.py
+++ Full_Discharge/Full_Discharge_USB.py
@@ -15,6 +15,7 @@ import winsound
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 2000  # Set Duration To 1000 ms == 1 second
 
+# test change
 
 ### User INPUT ###
 fileFolder = 'C:/Users/nwoodwa/Desktop/SolarPack/'
