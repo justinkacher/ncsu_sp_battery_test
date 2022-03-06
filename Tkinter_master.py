c339a115e585776985748aa12a45cb31ef6a1f94
justinkacher <justinkacher@gmail.com>
2022-03-06 11:38:38 -0500

    test

--- Tkinter_master.py
+++ Tkinter_master.py
@@ -6,11 +6,13 @@ from tkinter.constants import *
 from tkinter.ttk import Labelframe
 
 import time
+
+from matplotlib.pyplot import title
 #from matplotlib import scale
-import tkcommands
+#import tkcommands
 import config
 import scaletest
-
+#from PIL import *
 
 
 
@@ -46,54 +48,54 @@ def refreshscale():
 
 
 
-def starttest1():
-    tkcommands.checkstate1()
-    errorlbl.configure(
-        text = config.msgsite1
-    )
-    ID2num.configure(
-        text = config.IDsite1
-    )
-    massvallbl.configure(
-        text = config.scalevalue
-    )
-    IDnum.configure(
-        text = config.IDscale
-    )
+# def starttest1():
+#     tkcommands.checkstate1()
+#     errorlbl.configure(
+#         text = config.msgsite1
+#     )
+#     ID2num.configure(
+#         text = config.IDsite1
+#     )
+#     massvallbl.configure(
+#         text = config.scalevalue
+#     )
+#     IDnum.configure(
+#         text = config.IDscale
+#     )
 
 
-def starttest2():
-    tkcommands.checkstate2()
-    errorlbl2.configure(
-        text = config.msgsite2
-    )
-    ID3num.configure(
-        text = config.IDsite2
-    )
-    massvallbl.configure(
-        text = config.scalevalue
-    )
-    IDnum.configure(
-        text = config.IDscale
-    )
+# def starttest2():
+#     tkcommands.checkstate2()
+#     errorlbl2.configure(
+#         text = config.msgsite2
+#     )
+#     ID3num.configure(
+#         text = config.IDsite2
+#     )
+#     massvallbl.configure(
+#         text = config.scalevalue
+#     )
+#     IDnum.configure(
+#         text = config.IDscale
+#     )
 
-def removetest1():
-    tkcommands.rmvcheckstate1()
-    errorlbl.configure(
-        text = config.msgsite1
-    )
-    ID2num.configure(
-        text = config.IDsite1
-    )
+# def removetest1():
+#     tkcommands.rmvcheckstate1()
+#     errorlbl.configure(
+#         text = config.msgsite1
+#     )
+#     ID2num.configure(
+#         text = config.IDsite1
+#     )
 
-def removetest2():
-    tkcommands.rmvcheckstate2()
-    errorlbl2.configure(
-        text = config.msgsite2
-    )
-    ID3num.configure(
-        text = config.IDsite2
-    )
+# def removetest2():
+#     tkcommands.rmvcheckstate2()
+#     errorlbl2.configure(
+#         text = config.msgsite2
+#     )
+#     ID3num.configure(
+#         text = config.IDsite2
+#     )
 
 
 
@@ -103,8 +105,8 @@ def removetest2():
 
 
 window = tk.Tk()
-window.title("18650 Battery Profile")
-window.geometry("1200x600")
+window.title("21700 Battery Profile")
+window.geometry("1200x700")
 window.resizable(0,0)
 window.grid_rowconfigure(1,weight = 1)
 window.grid_columnconfigure(2,weight = 1)
@@ -112,49 +114,66 @@ window.grid_columnconfigure(2,weight = 1)
 
 
 
+#title frame
+titleframe = tk.Frame(window,
+    width = 1200,
+    height = 100,
+    borderwidth=2,
+    relief = SUNKEN
+)
+titleframe.grid(row = 1, column = 1, sticky = N, pady = 0, padx = 0)
+titleframe.place(width = 1200, height = 100)
+
 #3 columns
 scaleframe = tk.Frame(window,
     width = 400,
     height = 800,
-    borderwidth=5,
-    relief = GROOVE
+    borderwidth=2,
+    relief = SUNKEN
 )
 scaleframe.grid(row = 1, column = 1,sticky = NS, pady = 0, padx = 0)
-scaleframe.place(width = 400,height = 800)
-#scaleframe.pack(side = LEFT)
-#scaleframe.place(
-#    anchor='c',
-#    relx = 1/6,
-#    rely = 1/2
-#)
+scaleframe.place(x = 0, y = 100, width = 400,height = 600)
+
+
 site1frame = tk.Frame(window,
     width = 400,
     height = 800,
-    borderwidth=5,
-    relief = GROOVE
+    borderwidth=2,
+    relief = SUNKEN
 )
 site1frame.grid(row = 1, column = 2,sticky = NS,pady = 0, padx = 0)
-site1frame.place(x=400,width = 400,height = 800)
-#site1frame.pack()
-#site1frame.place(
-#    anchor='c',
-#    relx = 3/6,
-#    rely = 1/2
-#)
+site1frame.place(x=400, y = 100, width = 400,height = 600)
+
+
 site2frame = tk.Frame(window,
     width = 400,
     height = 800,
-    borderwidth=5,
-    relief = GROOVE
+    borderwidth=2,
+    relief = SUNKEN
 )
 site2frame.grid(row = 1, column = 3,sticky = NS, pady = 0, padx = 0)
-site2frame.place(x=800,width = 400,height = 800)
-#site2frame.pack(side = RIGHT)
-#site2frame.place(
-#    anchor='c',
-#    relx = 5/6,
-#    rely = 1/2
-#)
+site2frame.place(x=800, y = 100, width = 400,height = 600)
+
+
+
+
+#title##########################################
+titlelbl = tk.Label(
+    titleframe,
+    text = "21700 Battery Testing",
+    anchor = CENTER,
+    font = ('Arial',35)
+)
+titlelbl.place(y = 20, width = 1200)
+
+
+# canvas = Canvas(titleframe, width = 100, height = 100)
+# canvas.pack()
+# logo = ImageTk.PhotoImage('Logo.png')
+# canvas.create_image(20,20, anchor = NW, image = logo)
+
+
+
 
 
 #scale #########################################
@@ -185,7 +204,7 @@ tarebtn = tk.Button(
     scaleframe,
     text = "Tare",
     height = 5,
-    command = tarescale
+    #command = tarescale
     )
 tarebtn.grid(row = 4)
 tarebtn.place(x=25,y = 150,width = 350,height = 100)
@@ -194,7 +213,7 @@ readbtn = tk.Button(
     scaleframe,
     text = "Read",
     height = 5,
-    command = readscale
+    #command = readscale
 )
 readbtn.grid(row = 5)
 readbtn.place(x = 25,y=250,width = 350, height = 100)
@@ -252,7 +271,7 @@ start1btn = tk.Button(
     site1frame,
     text = "Start",
     bg = '#8dc989',
-    command = starttest1
+    #command = starttest1
 )
 start1btn.grid(row = 5)
 start1btn.place(y= 400,x=25,width = 150, height = 50)
@@ -261,7 +280,7 @@ remove1btn = tk.Button(
     site1frame,
     text = "Remove",
     bg = '#ab5454',
-    command = removetest1
+    #command = removetest1
 )
 remove1btn.grid(row = 5)
 remove1btn.place(y = 400,x=225,width = 150,height = 50)
@@ -311,7 +330,7 @@ start2btn = tk.Button(
     site2frame,
     text = "Start",
     bg = '#8dc989',
-    command = starttest2
+    #command = starttest2
 )
 start2btn.grid(row = 5)
 start2btn.place(y= 400,x=25,width = 150, height = 50)
@@ -320,7 +339,7 @@ remove2btn = tk.Button(
     site2frame,
     text = "Remove",
     bg = '#ab5454',
-    command = removetest2
+    #command = removetest2
 )
 remove2btn.grid(row = 5)
 remove2btn.place(y = 400,x=225,width = 150,height = 50)
