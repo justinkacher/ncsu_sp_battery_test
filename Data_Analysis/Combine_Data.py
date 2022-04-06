594b890def0368cc6db6206020c23c7af3bc965f
nrwoodward <nwoodwa@ncsu.edu>
2022-04-05 21:05:11 -0400

    finished combined_data

--- Data_Analysis/Combine_Data.py
+++ Data_Analysis/Combine_Data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import json
 
 
 
@@ -9,90 +10,100 @@ import numpy as np
 
 
 
-folder = ' '
+folder = 'C:/Users/nwoodwa/Documents/SolarPack'
 
 # battery serial number is test
 ## tests are saved using: df_battery_dict.to_excel(fileFolder + '/Test cell ' + cell_Dict['Cell Number'] + '.xlsx') # line 113 Tkinter_master
-fileNames = os.listdir(folder)    # returns list of file name and .Type for specified folder
+folderContents = os.listdir(folder)    # returns list of folder and file name .Type for the specified folder
+print(folderContents)
 
+new_Files = []
+for item in folderContents:
+    if item[-4:] == "xlsx":
+        new_Files.append(item)
+    # cell_number = file[-16:-5]
+    # update file with only new cells tested
+    # get master file names
+    # compare all fileNames to master file names
+    # if not in master: add data
 
-# update file with only new cells tested
-# get master file names
-# compare all fileNames to master file names
-# if not in master: add data
-new_Files = fileNames
-
-
-
+print(new_Files)
 
 # open master files
 # xls = pd.ExcelFile('{}/{}.xlsx'.format(folder, file))
 # df_measurements = pd.read_excel(xls, 'Voc')
 
 
-df_measurements = {}
-resitance_dict = {}
-
-
-
 ##df4 = df4.reindex(range( max(len(df4), len(c)) ))
 ##df4['three'] = pd.Series(c)
-
-
 
 
 # open test file and save data to master
 # file name: '/Test cell ' + cell_Dict['Cell Number'] + '.xlsx'
 # Data:
+# cell number
 # cell_Dict['Analog Impedence'] is a float
 # cell_Dict['Voc (V)'] is a float
 # cell_Dict['DC Resistance (Ohms)'] is a float
 # cell_Dict.update({'Capacity Time': time_list, 'Capacity Voltage': voltage_list})
 
-impedance = {}
-voc = {}
-resistance = {}
-cap_volt_list = {}
-cap_time_list = {}
+master = {'impedance': {}, 'voc': {}, 'resistance': {}, 'cap_volt': {}, 'cap_time':{}}
+skipped = []
 
 for file in new_Files:
-    cell_number = file[-16:-5]  # Test cell [cell number]
-
-    xls = pd.ExcelFile('{}/{}.xlsx'.format(folder, file))
-    df_measurements = pd.read_excel(xls)
-
-    impedance[cell_number] = df_measurements['Analog Impedence']
-    voc[cell_number] = df_measurements['Voc (V)']
-    resistance[cell_number] = df_measurements['DC Resistance (Ohms)']
-    cap_volt_list[cell_number] = df_measurements['Capacity Voltage']
-    cap_time_list[cell_number] = df_measurements['Capacity Time']
-
-
-
-
-
-
-
-
-skipped = []        # list of files skipped due to error
-
-# run Dual analysis for each file
-for file in fileNames:
-    print(file)
-    filePath = folder + "/" + file
-    savePath = folder + '/Corrected V0.2/' + file[:-5]         # [:-5] removes the .xlsx from the name
-
     try:
-        dam.daul_analysis(filePath, savePath)
+        xls = pd.ExcelFile('{}/{}'.format(folder, file))
+        df_measurements = pd.read_excel(xls)
+
+        cell_number = df_measurements['Cell Number'].iloc[0]
+        # cell_number = file[-16:-5]  # Test cell [cell number]
+
+        master['impedance'][cell_number] = df_measurements['Analog Impedence'].iloc[0]
+        master['voc'][cell_number] = df_measurements['Voc (V)'].iloc[0]
+        master['resistance'][cell_number] = df_measurements['DC Resistance (Ohms)'].iloc[0]
+        master['cap_volt'][cell_number] = df_measurements['Capacity Voltage'].to_list()
+        master['cap_time'][cell_number] = df_measurements['Capacity Time'].to_list()
     except:
-        print("Skipped")
         skipped.append(file)
 
 print(skipped)
 
+print(cell_number)
 
-## Saving
+print(master['impedance'][cell_number])
+print(master['voc'][cell_number])
+print(master['resistance'][cell_number])
+print(master['cap_volt'][cell_number])
+print(master['cap_time'][cell_number])
 
-# Serializing json
-# json_object = json.dumps(dictionary, indent = 4)
+
+print(master)
+
+
+
+#
+#
+# # run Dual analysis for each file
+# for file in fileNames:
+#     print(file)
+#     filePath = folder + "/" + file
+#     savePath = folder + '/Corrected V0.2/' + file[:-5]         # [:-5] removes the .xlsx from the name
+#
+#     try:
+#         dam.daul_analysis(filePath, savePath)
+#     except:
+#         print("Skipped")
+#         skipped.append(file)
+#
+# print(skipped)
+#
+#
+# ## Saving
+#
+# create json
+# json_object = json.dumps(master, indent=4)
 # print(json_object)
+
+# save .json
+with open(folder +'/'+"Master_Data.json", "w") as outfile:
+    json.dump(master, outfile)
