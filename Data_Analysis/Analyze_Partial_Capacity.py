b8a127c559e6edb76b3d4d8f4a4c36a160f0a622
nrwoodward <68925257+nrwoodward@users.noreply.github.com>
2022-05-21 12:00:23 -0400

    calculate slope

--- Data_Analysis/Analyze_Partial_Capacity.py
+++ Data_Analysis/Analyze_Partial_Capacity.py
@@ -6,7 +6,7 @@ import statistics
 import math
 
 
-folder = '/Volumes/GoogleDrive-115458662593069358043/Shared drives/SolarPack/Teams/Electrical/Project Teams/Battery/Battery Test Plan/Partial Discharge/All Tested through April 23/partial_discharge'
+folder = '/Volumes/GoogleDrive-115458662593069358043/Shared drives/SolarPack (1)/Teams/Electrical/Project Teams/Battery/Battery Test Plan/Tested Batteries/partial_discharge'
 masterFile = folder + '/Master_Data.json'
 
 # folder = 'C:/Users/nwoodwa/Documents/SolarPack'
@@ -27,21 +27,32 @@ cells_tested = list(master['cap_volt'].keys())
 # Voltage vs Time
 # it is assumed that a discharges
 
-cap_slope = []
-for cell in cells_tested:
+capacityTest_dict = {}		# dictionary of each cell's capacity analysis; more dictionaries of voltage drop and slope calculation
+for cell in cells_tested[:15]:
+	cell_dict = {}
+
 	cap_volt = master['cap_volt'][cell]		# list of voltages
 	cap_time = master['cap_time'][cell]		# list of times
 
 	# slope =  dy / dx = ( volt[i+1] - volt[i] ) / ( cap_time[] )
 	slope = []
-	for i in range(len(cap_volt)):
+	for i in range(len(cap_volt)-1):
 		slope_i = ( cap_volt[i+1] - cap_volt[i] ) / ( cap_time[i+1] - cap_time[i] )
 		slope.append(slope_i)
 
-	print(statistics.mean(slope))
-	print(cap_volt[-1] - cap_volt[0] ) / ( cap_time[-1] - cap_time[0])
+	mean_slope = statistics.mean(slope)
+	total_slope = (cap_volt[-1] - cap_volt[0]) / ( cap_time[-1] - cap_time[0])
+	volt_drop = cap_volt[-1] - cap_volt[0]
 
-	cap_slope.append(statistics.mean(slope))
+	cell_dict['mean slope'] = mean_slope
+	cell_dict['total slope'] = total_slope
+	cell_dict['volt drop'] = volt_drop
+
+	capacityTest_dict[cell] = cell_dict
+
+print(capacityTest_dict)
+
+cap_json = json.dumps(capacityTest_dict, indent=4)
 
 
 
